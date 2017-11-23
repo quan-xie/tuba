cdfab44afe645240a29e301b49cf0bae799e5d5d
谢权 <i@xiequan.info>
2017-11-23 15:33:29 +0800

    modify http

--- transport/http.go
+++ transport/http.go
@@ -4,16 +4,29 @@ import (
 	"crypto/tls"
 	"net"
 	"net/http"
+	"time"
 )
 
+type ClientConfig struct {
+	Dial      time.Duration
+	Timeout   time.Duration
+	KeepAlive time.Duration
+}
+
 type Client struct {
+	conf      *ClientConfig
 	client    *http.Client
 	dialer    *net.Dialer
 	transport *http.Transport
 }
 
-func NewClient() *Client {
+func NewClient(c *ClientConfig) *Client {
 	client := new(Client)
+	client.conf = c
+	client.dialer = &net.Dialer{
+		Timeout:   time.Duration(c.Dial),
+		KeepAlive: time.Duration(c.KeepAlive),
+	}
 	client.transport = &http.Transport{
 		DialContext:     client.dialer.DialContext,
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
