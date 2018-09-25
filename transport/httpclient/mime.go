fe98d4a6234a152205d2b86fd3d2173c0b7eb2c0
xie <quan.xie@andui.com>
2018-09-26 02:08:02 +0800

    modify httpclient

---
+++ transport/httpclient/mime.go
@@ -0,0 +1,14 @@
+package httpclient
+
+const (
+	MIMEJSON              = "application/json"
+	MIMEHTML              = "text/html"
+	MIMEXML               = "application/xml"
+	MIMEXML2              = "text/xml"
+	MIMEPlain             = "text/plain"
+	MIMEPOSTForm          = "application/x-www-form-urlencoded"
+	MIMEMultipartPOSTForm = "multipart/form-data"
+	MIMEPROTOBUF          = "application/x-protobuf"
+	MIMEMSGPACK           = "application/x-msgpack"
+	MIMEMSGPACK2          = "application/msgpack"
+)
