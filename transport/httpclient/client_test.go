8bb826aa39a4b42004497aae37ea752d6ee5b64a
xiequan <xiequan@qutoutiao.net>
2019-06-07 16:03:40 +0800

    add http test file

--- transport/httpclient/client_test.go
+++ transport/httpclient/client_test.go
@@ -1,1 +1,44 @@
 package httpclient
+
+import (
+	"testing"
+	"time"
+
+	"github.com/quan-xie/tuba/util/xtime"
+	"golang.org/x/net/context"
+)
+
+var (
+	cfg    *Config
+	client *HttpClient
+)
+
+func init() {
+	cfg = &Config{
+		Dial:       xtime.Duration(time.Second),
+		Timeout:    xtime.Duration(time.Second),
+		KeepAlive:  xtime.Duration(time.Second),
+		retryCount: 2,
+	}
+	client = NewHTTPClient(cfg)
+}
+
+func TestHttpClient_Get(t *testing.T) {
+	var res interface{}
+	err := client.Get(context.Background(), "http://httpbin.org/get", nil, &res)
+	if err != nil {
+		t.Log(err)
+	}
+	t.Log(res)
+
+}
+
+func TestHttpClient_Post(t *testing.T) {
+	var res interface{}
+	param := make(map[string]interface{})
+	err := client.Post(context.Background(), "http://httpbin.org/post", MIMEJSON, nil, param, &res)
+	if err != nil {
+		t.Log(err)
+	}
+	t.Log(res)
+}
