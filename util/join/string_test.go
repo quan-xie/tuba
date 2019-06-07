8962b66502b0911623e97e283b9f7af43df43d14
谢权 <i@xiequan.info>
2019-06-08 07:31:03 +0800

    add log test

--- util/join/string_test.go
+++ util/join/string_test.go
@@ -3,9 +3,9 @@ package join
 import "testing"
 
 func TestJoinInts(t *testing.T) {
-
+	t.Log(nil)
 }
 
 func TestSplitInts(t *testing.T) {
-
+	t.Log(nil)
 }
--- util/xtime/time_test.go
+++ util/xtime/time_test.go
@@ -3,13 +3,13 @@ package xtime
 import "testing"
 
 func TestDuration_UnmarshalText(t *testing.T) {
-
+ t.Log(nil)
 }
 
 func TestTime_Scan(t *testing.T) {
-
+	t.Log(nil)
 }
 
 func TestTime_Value(t *testing.T) {
-
+	t.Log(nil)
 }
