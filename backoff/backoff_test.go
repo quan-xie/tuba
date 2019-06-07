3217845fc57d8844c682e7be8841e6d155df3d32
谢权 <i@xiequan.info>
2019-06-08 06:30:36 +0800

    use zap log

--- backoff/backoff_test.go
+++ backoff/backoff_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestExponentialBackoff_Next(t *testing.T) {
 	backoff := NewExponentialBackoff(time.Second, 2*time.Second, 2.0)
-	for i := 0; i <= 10; i++ {
+	for i := 0; i <= 5; i++ {
 		fmt.Printf("Interval %d \n", backoff.Next(i))
 		time.Sleep(backoff.Next(i))
 	}
@@ -18,7 +18,7 @@ func TestExponentialBackoff_Next(t *testing.T) {
 
 func TestConstantBackoff_Next(t *testing.T) {
 	backoff := NewConstantBackoff(xtime.Duration(time.Second))
-	for i := 0; i <= 10; i++ {
+	for i := 0; i <= 5; i++ {
 		fmt.Printf("Interval %d \n", backoff.Next(i))
 		time.Sleep(backoff.Next(i))
 	}
--- log/log.go
+++ log/log.go
@@ -1,118 +1,62 @@
 package log
 
 import (
-	"fmt"
-	"math"
-	"os"
 	"time"
 
-	"github.com/Sirupsen/logrus"
-	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-var xlog = logrus.New()
+var logger *zap.Logger
 
 type Config struct {
-	Dir      string
-	Logstash *Logstash
+	OutputPaths []string
+	ErrorOutputPaths []string
+	Development bool
 }
 
-// Init is Initial log config .
-func Init(conf *Config) {
-	if conf.Dir != "" {
-		pathMap := PathMap{
-			logrus.InfoLevel:  conf.Dir + "/info.log",
-			logrus.ErrorLevel: conf.Dir + "/error.log",
-			logrus.WarnLevel:  conf.Dir + "/warn.log",
-		}
-		xlog.Hooks.Add(NewLocalHook(
-			pathMap,
-			&logrus.JSONFormatter{},
-		))
-	}
-	logStash, err := NewLogstash(conf.Logstash)
+func Init(c *Config) {
+	var err error
+	cfg := zap.NewProductionConfig()
+	cfg.OutputPaths = c.OutputPaths
+	cfg.ErrorOutputPaths=c.OutputPaths
+	cfg.Development = c.Development
+	cfg.DisableStacktrace=true
+	cfg.DisableCaller=true
+	cfg.EncoderConfig.EncodeTime = TimeEncoder
+	logger, err = cfg.Build()
+	logger.WithOptions()
 	if err != nil {
-		xlog.Error("NewLogstash error(%v)", err)
-	} else {
-		xlog.Hooks.Add(logStash)
+		return
 	}
+	defer logger.Sync()
+}
+
+func TimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format(time.RFC3339Nano))
 }
 
 // Info send log info to logstash
-func Info(format string, args ...interface{}) {
-	xlog.SetLevel(logrus.InfoLevel)
-	xlog.Infof(format, args...)
+func Info(msg string, fields ...zap.Field) {
+		logger.Info(msg, fields...)
 }
 
 // Error send log error to logstash
-func Error(format string, args ...interface{}) {
-	xlog.SetLevel(logrus.ErrorLevel)
-	xlog.Errorf(format, args...)
+func Error(msg string, fields ...zap.Field) {
+	logger.Error(msg, fields...)
 }
 
 // Warn send log warn to logstash
-func Warn(format string, args ...interface{}) {
-	xlog.SetLevel(logrus.WarnLevel)
-	xlog.Warnf(format, args...)
+func Warn(msg string, fields ...zap.Field) {
+	logger.Warn(msg, fields...)
 }
 
 // Debug send log debug to logstash
-func Debug(format string, args ...interface{}) {
-	xlog.SetLevel(logrus.DebugLevel)
-	xlog.Debugf(format, args...)
+func Debug(msg string, fields ...zap.Field) {
+	logger.Debug(msg, fields...)
 }
 
 // Fatal send log fatal to logstash
-func Fatal(format string, args ...interface{}) {
-	xlog.SetLevel(logrus.FatalLevel)
-	xlog.Fatalf(format, args...)
-}
-
-// Logger is the logrus logger handler
-func Ginlog() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// other handler can change c.Path so:
-		path := c.Request.URL.Path
-		start := time.Now()
-		c.Next()
-		stop := time.Since(start)
-		latency := int(math.Ceil(float64(stop.Nanoseconds()) / 1000000.0))
-		statusCode := c.Writer.Status()
-		clientIP := c.ClientIP()
-		clientUserAgent := c.Request.UserAgent()
-		referer := c.Request.Referer()
-		hostname, err := os.Hostname()
-		if err != nil {
-			hostname = "unknow"
-		}
-		dataLength := c.Writer.Size()
-		if dataLength < 0 {
-			dataLength = 0
-		}
-
-		entry := logrus.NewEntry(xlog).WithFields(logrus.Fields{
-			"hostname":   hostname,
-			"statusCode": statusCode,
-			"latency":    latency, // time to process
-			"clientIP":   clientIP,
-			"method":     c.Request.Method,
-			"path":       path,
-			"referer":    referer,
-			"dataLength": dataLength,
-			"userAgent":  clientUserAgent,
-		})
-
-		if len(c.Errors) > 0 {
-			entry.Error(c.Errors.ByType(gin.ErrorTypePrivate).String())
-		} else {
-			msg := fmt.Sprintf("%s - %s [%s] \"%s %s\" %d %d \"%s\" \"%s\" (%dms)", clientIP, hostname, time.Now().Format(time.RFC3339), c.Request.Method, path, statusCode, dataLength, referer, clientUserAgent, latency)
-			if statusCode > 499 {
-				entry.Error(msg)
-			} else if statusCode > 399 {
-				entry.Warn(msg)
-			} else {
-				entry.Info(msg)
-			}
-		}
-	}
+func Fatal(msg string, fields ...zap.Field) {
+	logger.Fatal(msg, fields...)
 }
