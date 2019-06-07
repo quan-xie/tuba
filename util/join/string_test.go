bf91684f4b1461771ba3d4a9907b09e0b4824bff
谢权 <i@xiequan.info>
2019-06-08 07:11:55 +0800

    add test file

---
+++ util/join/string_test.go
@@ -0,0 +1,11 @@
+package join
+
+import "testing"
+
+func TestJoinInts(t *testing.T) {
+
+}
+
+func TestSplitInts(t *testing.T) {
+
+}
---
+++ util/xtime/time_test.go
@@ -0,0 +1,15 @@
+package xtime
+
+import "testing"
+
+func TestDuration_UnmarshalText(t *testing.T) {
+
+}
+
+func TestTime_Scan(t *testing.T) {
+
+}
+
+func TestTime_Value(t *testing.T) {
+
+}
