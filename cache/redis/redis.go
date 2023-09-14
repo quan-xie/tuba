8a3a8cd8dcf0c64566b381d0b74f7a36706133ee
xiequan <i@xiequan.info>
2023-09-14 16:28:46 +0800

    update

--- cache/redis/redis.go
+++ cache/redis/redis.go
@@ -1,13 +1,8 @@
 package redis
 
 import (
-	"context"
-	"fmt"
-
 	"github.com/pkg/errors"
 	"github.com/quan-xie/tuba/log"
-	"github.com/redis/go-redis/v9"
-	"go.opencensus.io/trace"
 )
 
 func NewRedis(config *Config) (client *RedisStorage, err error) {
@@ -25,21 +20,4 @@ func NewRedisClient(config *Config) (client *RedisStorage) {
 		log.Fatalf("NewRedisClient error %v", err)
 	}
 	return
-}
-
-// PerCommandTracer provides the instrumented WrapProcess function that you can attach to any
-// client. It specifically takes in a context.Context as the first argument because
-// you could be using the same client but wrapping it in a different context.
-func PerCommandTracer(ctx context.Context) func(oldProcess func(cmd redis.Cmder) error) func(redis.Cmder) error {
-	return func(fn func(cmd redis.Cmder) error) func(redis.Cmder) error {
-		return func(cmd redis.Cmder) error {
-			_, span := trace.StartSpan(ctx, fmt.Sprintf("redis-go/%s", cmd.Name()))
-			defer span.End()
-			err := fn(cmd)
-			if err != nil {
-				span.SetStatus(trace.Status{Code: int32(trace.StatusCodeUnknown), Message: err.Error()})
-			}
-			return err
-		}
-	}
 }
